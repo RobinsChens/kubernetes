66aeda62e2f4080c1086fc76f410072eee23c7b2
Chen Wei <chen.wei@vpclub.cn>
2016-09-14 20:04:44 +0800

    modify fluentd image

---
+++ cluster/addons/fluentd-elasticsearch/fluentd-es-image/run.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+echo ELASTICSEARCH_URL=${ELASTICSEARCH_URL}
+echo "ELASTICSEARCH_URL is ${ELASTICSEARCH_URL}"
+sed -i "s/\${ELASTICSEARCH_URL}/${ELASTICSEARCH_URL}/g" /etc/td-agent/td-agent.conf
+/etc/init.d/td-agent start 
