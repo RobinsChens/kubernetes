19a003c2b6a196e8f98478ef0beb35f091f452b6
Chen Wei <waj_615@qq.com>
2016-09-18 20:24:57 +0800

    rm big src

---
+++ cluster/addons/efk/create-efk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+kubectl create -f service-account.yaml
+
+kubectl create -f kibana-service.yaml
+kubectl create -f elasticsearch-logging.yaml
+
+ELASTICSEARCH_HOST=$(kubectl --namespace=kube-system get svc elasticsearch -o template --template={{.spec.clusterIP}})
+echo "host is $ELASTICSEARCH_HOST"
+
+#replace
+sed -i -- "s/\${ELASTICSEARCH_HOST}/${ELASTICSEARCH_HOST}/g" fluentd-es-logging.yaml
+
+sed -i -- "s/\${ELASTICSEARCH_HOST}/${ELASTICSEARCH_HOST}/g" kibana-controller.yaml
+
+kubectl create -f fluentd-es-logging.yaml
+kubectl create -f kibana-controller.yaml
